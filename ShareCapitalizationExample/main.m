f15b78ee640f1de570a0907d9825ceb187616996
javaad.ali.ny@gmail.com <javaad101>
2024-12-05 07:36:23 -0600

    Initial Commit

---
+++ ShareCapitalizationExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ShareCapitalizationExample
+//
+//  Created by AdvocatesClose on 12/5/24.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+	return 0;
+}
